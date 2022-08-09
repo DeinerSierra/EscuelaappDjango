03c6a0a5464a3e54bec84743b2ca5f32d7e1b3c5
DeinerSierra <deiner.sierra@gmail.com>
2022-08-09 13:18:36 -0500

    Commit inicial

---
+++ escuelaapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from escuelaapp.models import *
+
+admin.site.register(Profesor)
+admin.site.register(Estudiante)
+admin.site.register(Programa)
+admin.site.register(Materia)
+admin.site.register(Seguimiento)
